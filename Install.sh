8d3b5c6dadedf6a7874d20cb366b2586e4e75a30
mukund20 <mukundatolay20@gmail.com>
2021-05-16 18:21:06 +0530

    added Install.sh

---
+++ Install.sh
@@ -0,0 +1,15 @@
+
+LOG= /tmp/student.log
+rm -f $LOG
+
+echo "WEB SERVER SETUP"
+echo "INSTALL WEB SERVER"
+
+yum intall nginx -y &>>$LOG
+
+if [ $? -eq 0 ]; then 
+    echo " - SUCCESS"
+else
+    echo " - FAILURE"
+    exit 1
+fi

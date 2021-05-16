2e743b9ab50f42f2cb36dee29318128bf7ccaf0a
mukund20 <mukundatolay20@gmail.com>
2021-05-16 19:50:17 +0530

    --- Auto-Git Commit ---

--- Install.sh
+++ Install.sh
@@ -23,6 +23,11 @@ fi
 }
 
 ## Main Program
+USER_ID = $(id -u)
+if [ $USER_ID-nq 0 ]; then 
+    echo- -e " You Should be root user to proceed!!"
+    exit 1
+fi
 
 Head "WEB SERVER SETUP"
 print "INSTALL WEB SERVER"

4ea0a387f74326b433b9bd02aa3fc069e1dbe101
mukund20 <mukundatolay20@gmail.com>
2021-05-16 19:26:39 +0530

    --- Auto-Git Commit ---

--- Install.sh
+++ Install.sh
@@ -3,12 +3,12 @@ LOG=/tmp/student.log
 rm -f $LOG
 
 echo "WEB SERVER SETUP"
-echo "INSTALL WEB SERVER"
+echo -n "INSTALL WEB SERVER"
 
 yum install nginx -y &>>$LOG
 
 if [ $? -eq 0 ]; then 
-    echo " - SUCCESS"
+    echo  " - SUCCESS"
 else
     echo " - FAILURE"
     exit 1

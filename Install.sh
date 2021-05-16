c6fdbb99c7c9c08c42645eedea37865a98bb1b9a
mukund20 <mukundatolay20@gmail.com>
2021-05-16 19:45:25 +0530

    --- Auto-Git Commit ---

--- Install.sh
+++ Install.sh
@@ -8,17 +8,24 @@ Head() {
     echo -e "\n\t\t\t\e[1;4;35m $1 \e[0m\n"
 }
 
-## Main Program
+print() {
+    echo -e "  $1 \t\t "
+}
 
-Head "WEB SERVER SETUP"
-echo -n "INSTALL WEB SERVER"
-
-yum install nginx -y &>>$LOG
-
-if [ $? -eq 0 ]; then 
+STAT_CHECK() {
+if [ $1 -eq 0 ]; then 
     echo  " - SUCCESS"
 else
     echo " - FAILURE"
     exit 1
 fi
+
+}
+
+## Main Program
+
+Head "WEB SERVER SETUP"
+print "INSTALL WEB SERVER"
+yum install nginx -y &>>$LOG
+STAT_CHECK $?
 
